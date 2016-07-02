c77e39bec96d168bcaeec98316d254084fa5f659
Luka Miljak <lmiljak@student.tudelft.nl>
2016-07-02 14:05:26 +0200

    Formatted the DowerTefence class.

--- src/main/java/com/github/LMiljak/DowerTefence/DowerTefence.java
+++ src/main/java/com/github/LMiljak/DowerTefence/DowerTefence.java
@@ -3,8 +3,8 @@ package com.github.LMiljak.DowerTefence;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Represents the Dower Tefence plugin.
- * This class should never be be instantiated apart from the Server running the Plugin.
+ * Represents the Dower Tefence plugin. This class should never be be
+ * instantiated apart from the Server running the Plugin.
  */
 public final class DowerTefence extends JavaPlugin {
 
@@ -12,10 +12,10 @@ public final class DowerTefence extends JavaPlugin {
 	public void onEnable() {
 		getLogger().info("The plugin has been successfully enabled");
 	}
-	
+
 	@Override
 	public void onDisable() {
 		getLogger().info("The plugin has been successfully disabled");
 	}
-	
+
 }
