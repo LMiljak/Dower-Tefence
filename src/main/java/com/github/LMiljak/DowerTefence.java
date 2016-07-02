c1d6bd02010f79b6b3b39f31756dfab7e711cfed
Luka Miljak <lmiljak@student.tudelft.nl>
2016-07-02 13:33:04 +0200

    Added the Plugin's class.
    Created the DowerTefence class, which represents the entire plugin.
    This class should never be instantiated apart from the Server running
    the Plugin.

---
+++ src/main/java/com/github/LMiljak/DowerTefence.java
@@ -0,0 +1,11 @@
+package com.github.LMiljak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Represents the Dower Tefence plugin.
+ * This class should never be be instantiated apart from the Server running the Plugin.
+ */
+public final class DowerTefence extends JavaPlugin {
+
+}
