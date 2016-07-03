ff22ed80cb9d7425513c3b19ebf4e363cdcced6c
Luka Miljak <luka.miljak.main@gmail.com>
2016-07-03 22:20:10 +0200

    Turned abstract class Game into an interface

--- src/main/java/com/github/lmiljak/dowertefence/Game.java
+++ src/main/java/com/github/lmiljak/dowertefence/Game.java
@@ -1,8 +1,25 @@
 package com.github.lmiljak.dowertefence;
 
+import java.util.Set;
+
+import org.bukkit.entity.Player;
+
 /**
  * Represents a game of Dower Tefence containing a number of players.
  */
-public abstract class Game {
+public interface Game {
+
+	/**
+	 * Gets the players currently in this game.
+	 * 
+	 * @return
+	 * 		The players currently in this game.
+	 */
+	Set<Player> getPlayers();
+	
+	/**
+	 * Starts the game.
+	 */
+	void start();
 	
 }
