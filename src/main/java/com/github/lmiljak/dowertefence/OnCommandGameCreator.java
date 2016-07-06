db8401998f90d718d72db75ca67fd93b27526468
Luka Miljak <luka.miljak.main@gmail.com>
2016-07-06 15:34:26 +0200

    Removed unused imports and fixed JavaDoc.

--- src/main/java/com/github/lmiljak/dowertefence/OnCommandGameCreator.java
+++ src/main/java/com/github/lmiljak/dowertefence/OnCommandGameCreator.java
@@ -1,24 +1,11 @@
 package com.github.lmiljak.dowertefence;
 
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 /**
  * A GameCreator that creates a game that may contain multiple people, when
  * certain commands have been executed by a user.
- * 
- * More specifically, a player can execute the start command, at which a new
- * StandardGame will be created. If the player invited others as well, those
- * other players must execute the accept command before the game is created.
  */
 public class OnCommandGameCreator implements GameCreator {
 
